caae1ea44d0177bfe4e1c7cf1aa2a452a206e467
archit-55 <architagrawal55@gmail.com>
2019-06-09 22:46:36 +0530

    Delete r.py

--- r.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-import webbrowser
-data=input("Search")
-webbrowser.open_new('http://www.google.com/search?q='+data)
-if data=='hello' :
-    webbrowser.open_new_tab('http://www.google.com/search?q=wwe')
-else:
-    print("bye")
