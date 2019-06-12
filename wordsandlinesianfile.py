a82ed18108703a4f2d3f1b15bb82843918382f6b
Archit Agrawal <architagrawal55@gmail.com>
2019-06-12 11:31:42 -0700

    wordsandlinesianfile.py

---
+++ wordsandlinesianfile.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+file_name=input("Enter filename:")
+f=open(file_name)
+data=f.read()
+words=len(data.split())
+num_lines=len(data.splitlines())
+print("no of words=",words)
+print(num_lines)
